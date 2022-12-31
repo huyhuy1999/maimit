39797711655ff285e02f998bd323acc69ad92606
huyhuy1999 <huydo5599@gmail.com>
2022-12-31 19:14:48 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'Mai Mít';
+const giftUrl = 'https://www.facebook.com/Phuongmai30';
+const eventName = 'Happy new year';
+const titleCard = 'Tặng Mai Mít';
+const contentCard = 'Chúc cậu và gia đình năm mới tràn ngập niềm vui và những nụ cười.Mong những điều đẹp nhất sẽ đến với cậu trong năm mới';
+const giftImage = 'mai.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
